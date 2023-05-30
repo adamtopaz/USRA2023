d558bf4aa99b0db5ff4ebd69396164759fcc7a1a
adamtopaz <github@adamtopaz.com>
2023-05-30 11:36:00 -0600

    add sorries for AB4+5

---
+++ USRA2023/ExactFunctor.lean
@@ -0,0 +1,35 @@
+import Mathlib.CategoryTheory.Limits.Preserves.Finite
+import Mathlib.CategoryTheory.Limits.Shapes.Products
+import Mathlib.CategoryTheory.Limits.Filtered
+import Mathlib.CategoryTheory.Abelian.Basic
+
+namespace CategoryTheory.Functor
+
+open CategoryTheory Limits
+
+variable {C : Type _} [Category C] {D : Type _} [Category D]
+
+class Exact (F : C ⥤ D) extends PreservesFiniteLimits F, PreservesFiniteColimits F
+
+instance (F : C ⥤ D) [PreservesFiniteLimits F] [PreservesFiniteColimits F] : Exact F where
+
+example (F : C ⥤ D) [Exact F] : PreservesFiniteLimits F := inferInstance
+example (F : C ⥤ D) [Exact F] : PreservesFiniteColimits F := inferInstance
+
+class AB4 (𝓐 : Type _) [Category.{v} 𝓐] [Abelian 𝓐] [HasCoproducts 𝓐] where
+  exact (α : Type v) : Exact (colim : (Discrete α ⥤ 𝓐) ⥤ 𝓐)
+
+instance (𝓐 : Type _) [Category.{v} 𝓐] [Abelian 𝓐] [HasCoproducts 𝓐] [AB4 𝓐] (α : Type v) : 
+  Exact (colim : (Discrete α ⥤ 𝓐) ⥤ 𝓐) := AB4.exact _
+
+instance (𝓐 : Type _) [Category.{v} 𝓐] [Abelian 𝓐] [HasCoproducts 𝓐] : 
+    HasFilteredColimitsOfSize.{v,v} 𝓐 := 
+  sorry
+
+class AB5 (𝓐 : Type _) [Category.{v} 𝓐] [Abelian 𝓐] [HasCoproducts 𝓐] where
+  exact (J : Type v) [SmallCategory J] [IsFiltered J] : Exact (colim : (J ⥤ 𝓐) ⥤ 𝓐)
+
+instance (𝓐 : Type _) [Category.{v} 𝓐] [Abelian 𝓐] [HasCoproducts 𝓐] [AB5 𝓐] : AB4 𝓐 := 
+  sorry
+
+end CategoryTheory.Functor
