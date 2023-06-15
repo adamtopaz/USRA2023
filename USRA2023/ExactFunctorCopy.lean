9af645d599fee305a979c87b33ea2c38a3e0a225
adamtopaz <github@adamtopaz.com>
2023-06-15 10:58:37 -0600

    add iso skeleton

---
+++ USRA2023/ExactFunctorCopy.lean
@@ -0,0 +1,92 @@
+import Mathlib.CategoryTheory.Limits.Preserves.Finite
+import Mathlib.CategoryTheory.Limits.Shapes.Products
+import Mathlib.CategoryTheory.Limits.Filtered
+import Mathlib.CategoryTheory.Limits.Constructions.LimitsOfProductsAndEqualizers
+import Mathlib.CategoryTheory.Abelian.Basic
+
+namespace CategoryTheory.Functor
+
+open CategoryTheory Limits
+
+variable {C : Type _} [Category C] {D : Type _} [Category D]
+
+class Exact (F : C ⥤ D) extends PreservesFiniteLimits F, PreservesFiniteColimits F
+
+instance (F : C ⥤ D) [PreservesFiniteLimits F] [PreservesFiniteColimits F] : Exact F where
+
+example (F : C ⥤ D) [Exact F] : PreservesFiniteLimits F := inferInstance
+example (F : C ⥤ D) [Exact F] : PreservesFiniteColimits F := inferInstance
+
+class AB4 (𝓐 : Type _) [Category.{v} 𝓐] [Abelian 𝓐] [HasCoproducts 𝓐] where
+  exact (α : Type v) : Exact (colim : (Discrete α ⥤ 𝓐) ⥤ 𝓐)
+
+instance (𝓐 : Type _) [Category.{v} 𝓐] [Abelian 𝓐] [HasCoproducts 𝓐] [AB4 𝓐] (α : Type v) : 
+  Exact (colim : (Discrete α ⥤ 𝓐) ⥤ 𝓐) := AB4.exact _
+
+class AB5 (𝓐 : Type _) [Category.{v} 𝓐] [Abelian 𝓐] [HasColimits 𝓐] where
+  exact (J : Type v) [SmallCategory J] [IsFiltered J] : Exact (colim : (J ⥤ 𝓐) ⥤ 𝓐)
+
+variable {C : Type _} [Category.{v} C] 
+
+example (α : Type _) (S : Set α) (a : α) (ha : a ∈ S) : S :=
+  ⟨_, ha⟩
+
+noncomputable abbrev coproductColimitDiagramMap {α : Type v} (X : α → C)
+    [HasFiniteCoproducts C] {S T : Finset α} (h : S ≤ T) : 
+    ∐ (fun s : S => X s) ⟶ ∐ (fun t : T => X t) := 
+  Sigma.desc fun s => Sigma.ι (fun t : T => X t) ⟨s.1, h s.2⟩ 
+
+@[simps]
+noncomputable
+def coproductColimitDiagram {α : Type v} (X : α → C)
+    [HasFiniteCoproducts C] : Finset α ⥤ C where
+  obj S := ∐ (fun s : S => X s)
+  map {S T : Finset α} (i : S ⟶ T) := coproductColimitDiagramMap X i.le
+
+@[simps]
+noncomputable
+def coproductColimitCocone {α : Type v} (X : α → C) [HasColimits C] : 
+    Cocone (coproductColimitDiagram X) where
+  pt := ∐ X
+  ι := {
+    app := fun S => show ∐ (fun s : S => X s) ⟶ ∐ X from 
+      Sigma.desc fun i => Sigma.ι _ i.1
+    naturality := sorry
+  }
+
+@[simps]
+noncomputable
+def coproductColimitCoconeIsColimit {α : Type v} (X : α → C) [HasColimits C] : 
+    IsColimit (coproductColimitCocone X) where
+  desc S := Sigma.desc fun a => 
+    letI e1 : X a ⟶ ∐ (fun b : ({a} : Finset α) => X b) := 
+      Sigma.ι (fun b : ({a} : Finset α) => X b) ⟨a, by simp⟩
+    letI e2 : ∐ (fun b : ({a} : Finset α) => X b) ⟶ S.pt := S.ι.app {a}
+    e1 ≫ e2
+  fac := sorry
+  uniq := sorry
+
+noncomputable
+def coproductIsoColimit {α : Type v} (X : α → C) [HasColimits C] : 
+    ∐ X ≅ colimit (coproductColimitDiagram X) := 
+  (coproductColimitCoconeIsColimit X).coconePointUniqueUpToIso (colimit.isColimit _)
+
+/-
+    where
+  hom := Sigma.desc fun a => 
+    letI e1 : X a ⟶ ∐ (fun b : ({a} : Finset α) => X b) := 
+      Sigma.ι (fun b : ({a} : Finset α) => X b) ⟨a, by simp⟩
+    letI e2 : ∐ (fun b : ({a} : Finset α) => X b) ⟶ colimit (coproductColimitDiagram X) := 
+      colimit.ι (coproductColimitDiagram X) {a}
+    e1 ≫ e2
+  inv := colimit.desc _ (coproductColimitCocone X)
+  hom_inv_id := sorry
+  inv_hom_id := sorry
+-/
+
+instance (𝓐 : Type _) [Category.{v} 𝓐] [Abelian 𝓐] [HasColimits 𝓐] [AB5 𝓐] : AB4 𝓐 := by
+  constructor
+  intro α
+  sorry
+
+end CategoryTheory.Functor
